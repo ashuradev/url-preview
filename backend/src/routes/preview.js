2029e831c21521d4274403fd6a5327f620c206ae
Ashura <the.lucas2016@gmail.com>
2019-10-25 20:36:42 -0200

    remover o service crawler

--- backend/src/routes/preview.js
+++ backend/src/routes/preview.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const crawl = require('./../services/crawler')
+const urlParser = require('url')
+const fetch = require('node-fetch')
+const domino = require('domino')
+
+const { createObjectFromEntries } = require('./../utils/collections')
 
 router.post('/preview', async (req, res) => {
     // Trata todos os errors gerados na preview da URL
@@ -19,7 +23,34 @@ router.post('/preview', async (req, res) => {
             throw err
         }
 
-        res.json(await crawl(req.body.url))
+        const body = await (await fetch(url)).text()
+
+        const document = domino.createWindow(body).document
+
+        const domain = urlParser.parse(url).hostname
+        
+        // Irá procurar todas as meta tags do OG no HTML e retornar um array
+        const openGraphElements = Array.from(document.querySelectorAll('meta[property*="og"]'))
+
+        // Aqui são os apelidos das propriedades das meta tags do OG
+        // Exemplo, o nome da propriedade site_name irá ser substituída por sitename
+        const aliases = {
+            site_name: 'sitename',
+            image: 'thumbnail'
+        }
+
+        const crawledData = createObjectFromEntries(openGraphElements.map(node => {
+            const property = node.getAttribute('property').replace('og:', '').replace(':', '_')
+            
+            const value = node.getAttribute('content')
+
+            return [
+                aliases[property] ? aliases[property] : property, 
+                value
+            ]
+        }))
+
+        crawledData.domain = domain;
 
     } catch (err) { 
         
